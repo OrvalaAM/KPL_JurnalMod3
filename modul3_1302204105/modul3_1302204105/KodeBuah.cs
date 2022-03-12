f52d1daaedc74b5da0e3d8be481bae059c465586
OrvalaAM <90945844+OrvalaAM@users.noreply.github.com>
2022-03-12 17:16:38 +0700

    menambahkan class PosisiKarakterGame

--- modul3_1302204105/modul3_1302204105/KodeBuah.cs
+++ modul3_1302204105/modul3_1302204105/KodeBuah.cs
@@ -10,7 +10,7 @@ namespace modul3_1302204105
     {
         string[] namaBuah = {"Apel", "Aprikot", "Alpukat", "Pisang", "Paprika", "Blackberry", "Ceri", 
             "Kelapa", "Jagung", "Kurma", "Durian", "Anggur", "Melon", "Semangka"};
-        string[] kodeBuah = {"A00", "B00", "C00", "D00", "E00", "F00", "G00", 
+        string[] kodeBuah = {"A00", "B00", "C00", "D00", "E00", "F00", 
             "H00", "I00", "J00", "K00", "L00", "M00", "N00", "O00"};
 
         public void getKodeBuah(String nama)
@@ -21,6 +21,7 @@ namespace modul3_1302204105
                 if(nama == namaBuah[i])
                 {
                     Console.WriteLine(kodeBuah[i]);
+                    break;
                 }
             }
         }
